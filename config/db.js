73c28ace42121e9df74d776b7b4cff38b848e2f2
Rahul Goyal <rahulgoyal7480@gmail.com>
2020-07-26 13:42:24 +0530

    Added:Profile GET and POST (create & update) route

--- config/db.js
+++ config/db.js
@@ -1,23 +1,24 @@
-const { Mongoose } = require('mongoose')
+const { Mongoose } = require('mongoose');
 
-const mongoose = require('mongoose')
-const config = require('config')
-const db = config.get('mongoURI')
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useCreateIndex: true,
       useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-    console.log('MongoDB connected....')
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+    console.log('MongoDB connected....');
   } catch (err) {
-    console.log(err.response)
+    console.log(err.response);
 
     //Exit process with failure
-    process.exit(1)
+    process.exit(1);
   }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
