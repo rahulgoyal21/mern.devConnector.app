0bb628ee6bb1a4986107c96e38554104275650ed
Rahul Goyal <rahulgoyal7480@gmail.com>
2020-08-08 18:48:00 +0530

    Added:Frontend setup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import Landing from './components/layout/landing';
+import Navbar from './components/layout/navbar';
+const App = () => (
+  <Fragment>
+    <Landing />
+    <Navbar />
+  </Fragment>
+);
+
+export default App;
