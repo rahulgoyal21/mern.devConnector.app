c4cd77c417af73f7ee3b4091b8602f7a0a37fcef
Rahul Goyal <rahulgoyal7480@gmail.com>
2020-08-15 23:19:35 +0530

    Added:PrivateRoute and current user profile

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,9 @@ import Navbar from './components/layout/Navbar';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
 import { loadUser } from './actions/auth';
+import PrivateRoute from './components/routing/PrivateRoute';
 import setAuthToken from './utils/setAuthToken';
 //Redux
 import { Provider } from 'react-redux';
@@ -31,6 +33,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
+
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
+  return <div> Dashboard</div>;
+};
+
+Dashboard.propTypes = {
+  auth: PropTypes.object.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.authReducer,
+  profile: state.profileReducer
+});
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
